6e466b8b133c42a4f4e548ecf1ec9a377c1e91c2
Freddy Söderberg <freddy.soderberg@edu.edugrade.se>
2022-02-27 14:52:25 +0100

    Testing commiting i Testing commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class App extends React.Component{
+   
+  // Constructor 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        items: [],
+        DataisLoaded: false
+    };
+}
+
+componentDidMount(){
+  const url = "http://api.github.com/users/FreddySoderberg/repos";
+  fetch(url)
+  .then((res) => res.json())
+  .then((json)=>{
+    this.setState({
+      items: json,
+      DataisLoaded: true
+    });
+  })
+}
+
+render(){
+  const { DataisLoaded, items} = this.state;
+  if(!DataisLoaded) return <div>
+    <h1>Loading.. Please wait</h1></div>;
+
+  return (
+    <div className = "App">
+      <h1> Fetch data from an api in react</h1> {
+        items.map((item) => (
+          <ol key = { item.id}>
+            name: {item.name},
+            description: {item.description},
+            id: {item.id}
+          </ol>
+        ))
+      }
+    </div>
+  );
+}
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </React.StrictMode>, 
+  document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
